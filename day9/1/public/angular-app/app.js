fbc1d2a1a76841c93272ec659c043fe42881590b
thenewcreation <galatafir@yahoo.com>
2021-06-30 17:49:51 -0500

    day9

--- day9/1/public/angular-app/app.js
+++ day9/1/public/angular-app/app.js
@@ -1,13 +1,13 @@
-angular.module("meanGames", ["ngRoute"]).config(config);
+angular.module("meanTransport", ["ngRoute"]).config(config);
 
 function config($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl: "angular-app/game-list/game-list.html",
-        controller: "GamesController",
+        templateUrl: "angular-app/user-list/user-list.html",
+        controller: "UsersController",
         controllerAs: "vm"
-    }).when("/games/:id", {
-        templateUrl: "angular-app/game-display/game.html",
-        controller: "GameController",
+    }).when("/users/:id", {
+        templateUrl: "angular-app/user-display/user.html",
+        controller: "UserController",
         controllerAs: "vm"
     })
 }
--- day9/2/public/angular-app/app.js
+++ day9/2/public/angular-app/app.js
@@ -1,13 +1,13 @@
-angular.module("meanTransport", ["ngRoute"]).config(config);
+angular.module("meanGames", ["ngRoute"]).config(config);
 
 function config($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl: "angular-app/user-list/user-list.html",
-        controller: "UsersController",
+        templateUrl: "angular-app/game-list/game-list.html",
+        controller: "GamesController",
         controllerAs: "vm"
-    }).when("/users/:id", {
-        templateUrl: "angular-app/user-display/user.html",
-        controller: "UserController",
+    }).when("/games/:id", {
+        templateUrl: "angular-app/game-display/game.html",
+        controller: "GameController",
         controllerAs: "vm"
     })
 }
